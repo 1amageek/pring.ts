1732396dade4c2498d160ce2efdfff55d0459b85
1amageek <tmy0x3@icloud.com>
2017-12-21 19:14:12 +0900

    add test

---
+++ test/pring.base.test.ts
@@ -0,0 +1,21 @@
+process.env.NODE_ENV = 'test';
+
+import { Pring } from "../pring"
+import * as FirebaseFirestore from '@google-cloud/firestore'
+import * as Document from './test_document'
+
+Pring.initialize({
+    projectId: 'sandbox-329fc',
+    keyFilename: '../sandbox-329fc-firebase-adminsdk-8kgnw-3a2693f6cb.json'
+})
+
+test("test", async () => {
+    const document = new Document.Document()
+    await document.save()
+    try {
+        let doc: Document.Document = await Document.Document.get(document.id)
+        expect(doc.array).toBe(document.array)
+    } catch(error) {
+        console.log(error)
+    }
+})
