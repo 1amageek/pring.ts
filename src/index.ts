9ec448a22627e366b8170f4a76a0c8714723652b
1amageek <tmy0x3@icloud.com>
2018-03-01 21:47:11 +0900

    fix

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import * as FirebaseFirestore from '@google-cloud/firestore'
+import * as firebase from 'firebase-admin'
 
 // pring export
 import { Base, property} from './base'
@@ -9,6 +10,6 @@ import { File } from './file'
 
 export { Base, property, SubCollection, NestedCollection, ReferenceCollection, File }
 export let firestore: FirebaseFirestore.Firestore
-export function initialize(options?: any) {
+export function initialize(options?: firebase.AppOptions) {
     firestore = new FirebaseFirestore.Firestore(options)
 }
