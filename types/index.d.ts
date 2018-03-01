687331ab96cb99e952acb90b7bbd7e45002421fb
1amageek <tmy0x3@icloud.com>
2018-03-01 11:53:31 +0900

    remove types

--- types/index.d.ts
+++
@@ -1,8 +0,0 @@
-import * as FirebaseFirestore from '@google-cloud/firestore';
-export * from './base';
-export * from './subCollection';
-export * from './nestedCollection';
-export * from './referenceCollection';
-export * from './file';
-export declare let firestore: FirebaseFirestore.Firestore;
-export declare function initialize(options?: any): void;
