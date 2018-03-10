5e5bda26c85f372b5def36d68e4353330376d0f5
1amageek <tmy0x3@icloud.com>
2018-03-10 14:17:28 +0900

    update

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,11 +1,12 @@
 import * as firebase from 'firebase-admin';
 import * as FirebaseFirestore from '@google-cloud/firestore';
+import { BatchType } from './batchable';
 import { Base, property } from './base';
 import { SubCollection } from './subCollection';
 import { NestedCollection } from './nestedCollection';
 import { ReferenceCollection } from './referenceCollection';
 import { File } from './file';
-export { Base, property, SubCollection, NestedCollection, ReferenceCollection, File };
+export { BatchType, Base, property, SubCollection, NestedCollection, ReferenceCollection, File };
 export declare let firestore: FirebaseFirestore.Firestore;
 export declare const initialize: (options?: firebase.AppOptions) => void;
 export declare const batch: () => FirebaseFirestore.WriteBatch;
