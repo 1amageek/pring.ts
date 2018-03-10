f55ee67edec0d36b7db3127fa22c695e20782cae
1amageek <tmy0x3@icloud.com>
2018-03-10 14:09:50 +0900

    add BatchType

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,14 @@ import * as firebase from 'firebase-admin'
 import * as FirebaseFirestore from '@google-cloud/firestore'
 
 // pring export
+import { BatchType } from './batchable'
 import { Base, property } from './base'
 import { SubCollection } from './subCollection'
 import { NestedCollection } from './nestedCollection'
 import { ReferenceCollection } from './referenceCollection'
 import { File } from './file'
 
-export { Base, property, SubCollection, NestedCollection, ReferenceCollection, File }
+export { BatchType, Base, property, SubCollection, NestedCollection, ReferenceCollection, File }
 export let firestore: FirebaseFirestore.Firestore
 export const initialize = (options?: firebase.AppOptions) => {
     firestore = new FirebaseFirestore.Firestore(options)
