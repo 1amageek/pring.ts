4333c1b9cf64e9546a2bfb99323f5de8313d9af5
1amageek <tmy0x3@icloud.com>
2018-09-05 18:04:16 +0900

    bugfix

--- sample/src/views/Home.vue
+++ sample/src/views/Home.vue
@@ -10,6 +10,7 @@ import { Component, Vue } from 'vue-property-decorator';
 import HelloWorld from '../components/HelloWorld.vue'; // @ is an alias to /src
 import firebase from 'firebase';
 import { User } from '../models/user';
+import { config } from "../../../test/config"
 @Component({
   components: {
     HelloWorld,
@@ -18,15 +19,8 @@ import { User } from '../models/user';
 export default class Home extends Vue {
 
   public created() {
-    const config = {
-      apiKey: "AIzaSyDnt449K4ylMzkIK7WlJNVYDyao-XL64QA",
-      authDomain: "salada-f825d.firebaseapp.com",
-      databaseURL: "https://salada-f825d.firebaseio.com",
-      projectId: "salada-f825d",
-      storageBucket: "salada-f825d.appspot.com",
-      messagingSenderId: "1066463947512",
-    };
     const app = firebase.initializeApp(config);
   }
+
 }
 </script>
